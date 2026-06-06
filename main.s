ca24c66326dcf5c01b3bec6805e8d849f601eb75
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-06 15:23:18 +0000

    doctor class initiated , the plan is understanding the flow for doctor section then using it for nurses and patients

--- main.s
+++ main.s
@@ -2,33 +2,84 @@ global _start
 bits 64
 section .rodata
 menu:
-    db "<=================hospital menu===============>" 10
+    db "<=================hospital menu===============>",10
     db "1) doctors" ,10
     db "2) nurses",10
     db "3) patients",10
     db "4)exit",10
 menu_len equ $ -menu
 doc_menu:
-    db "<=================doctors menu===============>" 10
+    db "<=================doctors menu===============>", 10
     db "1) add doctor" ,10
     db "2) search doctor",10
     db "3) delete doctor",10
     db "4)exit",10
-menu_len equ $ -menu
-menu:
-    db "<=================hospital menu===============>" 10
+doc_menu_len equ $ -doc_menu
+nur_menu:
+    db "<=================hospital menu===============>", 10
     db "1) add nurse" ,10
     db "2) search nurse",10
     db "3) elete nurse",10
     db "4)exit",10
-menu_len equ $ -menu
-menu:
-    db "<=================hospital menu===============>" 10
+nur_menu_len equ $ -nur_menu
+pat_menu:
+    db "<=================hospital menu===============>", 10
     db "1) add patient" ,10
     db "2) search patient",10
     db "3) delete patient",10
     db "4)exit",10
-menu_len equ $ -menu
+pat_menu_len equ $ -pat_menu
+
+doc_add_id:
+    db "enter the id ",10
+    
+docid_menu_len equ $ -doc_add_id
+
+doc_add_name:
+    db "enter the name ",10
+    
+docnam_menu_len equ $ -doc_add_name
+doc_add_age:
+    db "enter the age ",10
+    
+docage_menu_len equ $ -doc_add_age
+
+
+
+section .bss
+
+vec_data 
+
+struc doctor
+    .id resd 4
+    .name resb 64
+    .age resd 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 section .text
